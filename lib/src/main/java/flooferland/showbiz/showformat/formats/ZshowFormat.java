7bf2344d34c1af4d91ac962113645d566609a845
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2024-06-30 14:36:29 +0300

    a

--- lib/src/main/java/flooferland/showbiz/showformat/formats/ZshowFormat.java
+++ lib/src/main/java/flooferland/showbiz/showformat/formats/ZshowFormat.java
@@ -4,7 +4,10 @@ import flooferland.showbiz.showformat.IShowFormat;
 import flooferland.showbiz.showformat.ShowData;
 import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
 import org.apache.commons.compress.archivers.sevenz.SevenZFile;
+import org.apache.commons.compress.utils.SeekableInMemoryByteChannel;
+import org.apache.commons.io.IOUtils;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -15,26 +18,29 @@ public class ZshowFormat implements IShowFormat {
     // TODO: Make this read from a stream
     @Override
     public ShowData readFromStream(InputStream fileStream) throws IOException {
-        SevenZFile sevenZFile = SevenZFile.builder().get();
+        byte[] fileBytes = IOUtils.toByteArray(fileStream);
+        var byteChannel = new SeekableInMemoryByteChannel(fileBytes);
 
-        for (SevenZArchiveEntry entry : sevenZFile.getEntries()) {
-            InputStream stream = sevenZFile.getInputStream(entry);
-            switch (entry.getName()) {
-                case "audio.wav":
-                    // TODO: Read audio stream and stuff
-                    break;
-                default:
-                    break;
+        try (SevenZFile sevenZFile = SevenZFile.builder().setSeekableByteChannel(byteChannel).get()) {
+            for (SevenZArchiveEntry entry : sevenZFile.getEntries()) {
+                InputStream stream = sevenZFile.getInputStream(entry);
+                System.out.printf("Reading file \"%s\" from 7z%n", entry.getName());
+                switch (entry.getName()) {
+                    case "audio.wav":
+                        // TODO: Read audio stream and stuff
+                        break;
+                    default:
+                        break;
+                }
+                stream.close();
             }
-            stream.close();
         }
 
-        sevenZFile.close();
         return null;
     }
 
     @Override
     public InputStream writeToStream(ShowData format) {
-        throw new RuntimeException("Not implemented");
+        return null;
     }
 }
--- lib/src/test/java/flooferland/showbiz/showformat/LibraryTest.java
+++ lib/src/test/java/flooferland/showbiz/showformat/LibraryTest.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 // TODO: Add more error handling for the formats: Check if the version of the file matches (and exists), etc
 
 class LibraryTest {
@@ -17,42 +19,39 @@ class LibraryTest {
     
     @Test void test() {
         System.out.println();
+        ShowData showData = null;
 
         // Reading RSHW
         System.out.println("-- reading RR files");
         RshowFormat rshow = new RshowFormat();
         try {
             InputStream stream = getFile(RshowPath);
-            rshow.readFromStream(stream);
+            showData = rshow.readFromStream(stream);
             stream.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        assertTrue(showData != null, "Rshow data is null");
         
         // Writing native files
         System.out.println("-- writing native files");
         ZshowFormat zshow = new ZshowFormat();
-        try {
-            InputStream zStream = getFile(zPath);
-            zshow.writeToStream(zStream);
-            zStream.close();
+        try (InputStream zStream = zshow.writeToStream(showData)) {
+            // Reading native files
+            System.out.println("-- reading native files");
+            try {
+                /*InputStream binStream = getFile(IntermediatePath);
+                new BinShowFormat().readFromStream(binStream);
+                binStream.close();*/
+                //InputStream zStream = getFile(zPath);
+                new ZshowFormat().readFromStream(zStream);
+                zStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
-        // Reading native files
-        System.out.println("-- reading native files");
-        try {
-            /*InputStream binStream = getFile(IntermediatePath);
-            new BinShowFormat().readFromStream(binStream);
-            binStream.close();*/
-            InputStream zStream = getFile(zPath);
-            new ZshowFormat().readFromStream(zStream);
-            zStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        // assertTrue(classUnderTest.fromIntermediate(), "fromIntermediate should return 'true'");
         
         System.out.println();
     }

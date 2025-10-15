3459f9083b7aebf4533b3610c214e43898fd259d
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-10-15 05:37:01 +0300

    Fix publishing (hopefully)

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/FlooferLand/bizlib")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = System.getenv("USERNAME")
+                password = System.getenv("PASSWORD")
             }
         }
     }

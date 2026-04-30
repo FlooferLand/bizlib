e2d20a2fdfb7fabc48b6d54a885799de9e2ef37d
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2026-04-30 23:39:05 +0300

    Fix the bizshw test

--- src/test/kotlin/BizshwTest.kt
+++ src/test/kotlin/BizshwTest.kt
@@ -14,25 +14,26 @@ class BizshwTest : FunSpec({
             rshowData = RshowFormat().read(Files.newInputStream(Path("D:\\Animatronics\\Creative Engineering\\Show tapes (RR)\\64th Birthday.rshw")))
         }
 
+        // NOTE: 64th Birthday.rshw is 250 MBs
         // TODO: Convert audio to OGG
         test("Write and read") {
             /*val format = BizshwFormat()
             val data = RawShowData(
-                signal = rshowData.signal.clone(),
-                audio = rshowData.audio.clone(),
-                video = rshowData.video.clone(),
+                signal = rshowData.signal,
+                audio = rshowData.audio,
+                video = rshowData.video,
             )
 
             format.write(Path("./test/excluded/64th Birthday.bizshw"), data)
 
-            val newData = format.readFile(Path("./excluded/64th Birthday.bizshw"))
+            val newData = format.readFile(Path("./test/excluded/64th Birthday.bizshw"))
             newData.audio.size shouldBeExactly data.audio.size
             newData.signal.size shouldBeExactly data.signal.size
             newData.video.size shouldBeExactly data.video.size
 
-            newData.audio shouldBe data.audio
-            newData.signal shouldBe data.signal
-            newData.video shouldBe data.video*/
+            newData.audio contentEquals data.audio
+            newData.signal contentEquals data.signal
+            newData.video contentEquals data.video*/
         }
     }
 })
